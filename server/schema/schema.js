a6427921ac98dbf8b6fb63d31559879f167939ec
unknown <rhbappy456@gmail.com>
2022-07-14 12:58:45 +0600

    projectType update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -20,6 +20,12 @@ const projectType = new GraphQLObjectType({
         name: {type: GraphQLString},
         description: {type: GraphQLString},
         status: {type: GraphQLString},
+        client:{
+            type: clientType,
+            resolve(parent, args){
+                return clients.find(client=>client.id === parent.clientId)
+            }
+        }
     })
 })
 const RootQuery = new GraphQLObjectType({
